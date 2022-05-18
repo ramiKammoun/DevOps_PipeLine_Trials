33c89b920186d7065dd9292517830061eab39d86
ramiKammoun <rami.kammoun@insat.u-carthage.tn>
2022-05-18 18:27:18 +0100

    Actions changes
    it should fail (the test)

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// hello world Function
+const helloworld = () => {
+    return 'hello world';
+}
+
+module.exports = {helloworld}
---
+++ index.spec.js
@@ -0,0 +1,8 @@
+const {helloworld} =require ('./index.js');
+
+// hello world test
+describe('hello world', function() {
+    it('should return hello world', function(){
+        expect(helloworld()).toEqual('hello world!');
+    });
+});
