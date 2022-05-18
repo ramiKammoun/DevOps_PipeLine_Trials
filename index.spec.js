d147340b298528e20b0bd74ae35ffca5740a2f3f
ramiKammoun <rami.kammoun@insat.u-carthage.tn>
2022-05-18 19:42:16 +0100

    Final Decherization

--- index.spec.js
+++ index.spec.js
@@ -3,6 +3,6 @@ const {helloworld} =require ('./index.js');
 // hello world test
 describe('hello world', function() {
     it('should return hello world', function(){
-        expect(helloworld()).toEqual('hello world!');
+        expect(helloworld()).toEqual('hello world');
     });
 });
